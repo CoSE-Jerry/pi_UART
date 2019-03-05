13da5c6594236e3b44563c7eb8c0ac2afd2b96cb
Jerry@Starting_block <jerry@coseinstruments.com>
2019-03-05 11:17:53 -0600

    Update write.py

--- write.py
+++ write.py
@@ -4,14 +4,12 @@
 import time
 import serial
           
-print("start")
 ser = serial.Serial(port='/dev/ttyAMA0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
-print(" serial start")
 counter=0
           
       
 while 1:
-    ser.write(str.encode("test"))
+    ser.write(counter)
     print(counter)
     time.sleep(1)
     counter += 1
