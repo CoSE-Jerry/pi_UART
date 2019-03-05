6682714fba672cf2406e999fc7b78cecae9281a1
Jerry@Starting_block <jerry@coseinstruments.com>
2019-03-05 11:09:47 -0600

    Update write.py

--- write.py
+++ write.py
@@ -10,6 +10,6 @@ counter=0
           
       
 while 1:
-    ser.write('Write counter: %d \n'%(counter))
+    ser.write(str.encode('Write counter: %d \n'%(counter)))
     time.sleep(1)
     counter += 1
